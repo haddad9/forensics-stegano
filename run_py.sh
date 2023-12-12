7692d515523c116e89a23ae650faf7112037ed2d
haddad <haddad@haddad-macbook.local>
2023-12-12 13:25:32 +0700

    init

---
+++ run_py.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+directory="./tws"
+
+if [ -d "$directory" ]; then
+    # Initialize the result file
+    echo "" > "$directory"/res/tes.txt
+
+    # Iterate through files in the directory
+    for file in "$directory"/*.txt; do
+        if [ -f "$file" ]; then
+            # Perform "cat" operation on the file
+            # put the name of the file into var
+            filename=$(basename -- "$file")
+            
+            temp_file=$(mktemp)
+
+            # Append filename to the result file
+            echo "$filename" >> "$directory"/res/tes.txt
+
+            # Append contents of the file to the result file
+            awk '{ gsub(/[^[:print:][:space:]]/, ""); print }' "$file" >> "$directory"/res/tes.txt
+        fi
+    done
+else
+    echo "Directory '$directory' not found."
+fi
