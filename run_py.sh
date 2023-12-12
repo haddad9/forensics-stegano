61f609bb7b5e7085d4c43c752ee68bd06147c5d3
haddad <haddad@haddad-macbook.local>
2023-12-12 15:07:51 +0700

    add

--- run_py.sh
+++ run_py.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-directory="./tws"
+directory="./images"
 
 if [ -d "$directory" ]; then
     # Initialize the result file
-    echo "" > "$directory"/res/tes.txt
 
     # Iterate through files in the directory
-    for file in "$directory"/*.txt; do
+    for file in "$directory"/*.*; do
         if [ -f "$file" ]; then
             # Perform "cat" operation on the file
             # put the name of the file into var
             filename=$(basename -- "$file")
+            steghide extract -sf "images"/"$filename" -xf "$directory"/res/"$filename".txt -p ""
+            lsbsteg decode -i "$file" -o "$directory"/res/"$filename"
+            openstego extract -sf "$file" -p "" -xd "$directory"/res/"$filename"
+            stegoveritas "$file" -o "$directory"/res/"$filename"
             
+
+
             temp_file=$(mktemp)
             lsbsteg encode -i "$file" -o "$temp_file"
             # Append filename to the result file
